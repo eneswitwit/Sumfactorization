730d625ca97221a989446eb5dc211bd9f078e036
Enes Witwit <Witwit@Eness-MacBook-Air.local>
2017-01-20 15:24:45 +0100

    newtonMatrix

--- polynomial.cpp
+++ polynomial.cpp
@@ -11,6 +11,7 @@ class Polynomial{
     vector<double> coefficients[0];
     vector<double> sample_points[0][0];
     vector<double> sample_values[0];
+    vector<double> newtonMatrix[0][0];
 
     // Constructor
     Polynomial(int deg){
@@ -23,6 +24,22 @@ class Polynomial{
 
     void divided_differences(){}
 
+    double product(double x_val, int x,double y_val, int y){
+        if(x>0){
+            return (x_val - sample_points[x][1])*product(x_val,x-1,y_val,y);
+        }
+        else{
+            return 1;
+        }
+    }
+
+    void compute_newtonMatrix(double x_val, double y_val){
+        for(int i=0;i<nodes_counter+1;++i){
+            for(int j=0;j<nodes_counter+1;++j){
+                newtonMatrix[i,j]= product(x_val,j,y_val,i)
+            }
+        }
+    }
 
 };
 
