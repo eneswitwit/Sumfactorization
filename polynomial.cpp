5d248760626030c0c24e040e73661e666da3b850
Enes Witwit <Witwit@dhcp568-147.laptop-wlc.uni-heidelberg.de>
2017-01-29 21:24:47 +0100

    Updated comments and added coefficient vector for newton basis.

--- polynomial.cpp
+++ polynomial.cpp
@@ -5,10 +5,17 @@
 
 class Polynomial
 {
+    // Coefficient vector for monom basis
     std::vector<int> a_;
+
+    // Coefficient vector for newton basis
+    std::vector<int> n_;
+
+
 
   public:
 
+    // 1-D. Conversion to 2-D is necessary
     Polynomial()
     : a_(1, int())
     {}
@@ -97,23 +104,23 @@ class Polynomial
 
 int main()
 {
-        // testen des Standard-Konstruktors
+    // Testen des Standard-Konstruktors
     Polynomial p0;
     assert(p0.degree() == 0);
     assert(p0[0] == 0);
 
-        // testen des Konstruktors mit Gradangabe
+    // Testen des Konstruktors mit Gradangabe
     Polynomial pm(-1);
     assert(pm == p0);
 
     Polynomial p2(2);
     assert(p2.degree() == 2);
 
-        // testen des Konstruktors mit Koeffizientenarray
+    // Testen des Konstruktors mit Koeffizientenarray
     Polynomial p{1, 2, 3};
     assert(p.degree() == 2);
 
-        // testen der Werte der Koeffizienten
+    // Testen der Werte der Koeffizienten
     assert(p2[0] == 0);
     assert(p2[1] == 0);
     assert(p2[2] == 0);
@@ -122,8 +129,8 @@ int main()
     assert(p[1] == 2);
     assert(p[2] == 3);
 
-        // testen, dass die Polynom fuer verschiedene Argumente x
-        // korrekt ausgerechnet werden
+    // Testen, dass die Polynom fuer verschiedene Argumente x
+    // korrekt ausgerechnet werden
     assert(p0(1) == 0);
     assert(p0(2) == 0);
     assert(p0(3) == 0);
@@ -136,7 +143,7 @@ int main()
     assert(p(2) == 17);
     assert(p(3) == 34);
 
-        // testen der Polynom-Addition
+    // Testen der Polynom-Addition
     Polynomial pa1 = p + p;
     Polynomial pa1_expected{2, 4, 6};
     assert(pa1 == pa1_expected);
@@ -145,7 +152,7 @@ int main()
     Polynomial pa2_expected{4, 8, 3};
     assert(pa2 == pa2_expected);
 
-        // testen der Polynom-Multiplikation
+    // Testen der Polynom-Multiplikation
     Polynomial pm1 = p * p;
     assert(pm1.degree() == 4);
     assert(pm1(2) == p(2)*p(2));
@@ -157,17 +164,19 @@ int main()
     Polynomial pm2_expected{2, 10, 18, 18};
     assert(pm2 == pm2_expected);
 
-        // testen der ersten Ableitung
+    // Testen der ersten Ableitung
     Polynomial derivative1_expected{2, 6};
     assert(p.derivative()  == derivative1_expected);
     assert(p.derivative(1) == derivative1_expected);
 
-        // testen der zweiten bis vierten Ableitung
+    // Testen der zweiten bis vierten Ableitung
     Polynomial derivative2_expected{6};
     assert(p.derivative(2)  == derivative2_expected);
     Polynomial derivative3_expected{0};
     assert(p.derivative(3)  == derivative3_expected);
     assert(p.derivative(4)  == derivative3_expected);
+
+    // Testen der 
 
     std::cout << "Alle Tests erfolgreich!\n";
 }
