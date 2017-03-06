067043737cff4c98fd595675791ed5d62fb4f3ba
Enes Witwit <Witwit@dhcp658-196.laptop-wlc.uni-heidelberg.de>
2017-03-06 11:30:14 +0100

    Solved conflicts ... ... ...

--- Enes/Sumfactorization/Sumfactorization/include/polynomial.h
+++ Enes/Sumfactorization/Sumfactorization/include/polynomial.h
@@ -1,14 +1,11 @@
 #ifndef __POLYNOMIAL_H__
 #define __POLYNOMIAL_H__
-#include <algorithm>
-#include <cmath>
-
 
 template <int order, typename y_type>
 class Polynomial {
 public:
 
-    constexpr y_type eval_lagr(int i, y_type x, const std::array<y_type,order+1> & knots) const {
+    constexpr y_type eval_lagr(int i, y_type x, const std::array < y_type, order + 1 > & knots) const {
         y_type val = 1.;
         for (int j = 0; j <= order; j++) {
             if (i != j) {
@@ -18,13 +15,15 @@ public:
         return val;
     }
 
-    constexpr y_type approx_gradient(int i, y_type x, std::array<y_type,order+1> & knots) const {
-        y_type h=0;
-        h=sqrt(std::nextafter(h, h));
-        if (x!=0) {
-                h*=x;
-            }
-        val=(eval_lagr(i,x+h,& knots)-eval_lagr(i,x+h,& knots))/(2*h);
-}
+    constexpr y_type approx_gradient(int i, y_type x, std::array < y_type, order + 1 > & knots) const {
+        y_type h = 0;
+        h = sqrt(std::nextafter(h, h));
+        if (x != 0) {
+            h *= x;
+        }
+        y_type val = (eval_lagr(i, x + h, & knots) - eval_lagr(i, x + h, & knots)) / (2 * h);
+    }
+
+};
 
 #endif
