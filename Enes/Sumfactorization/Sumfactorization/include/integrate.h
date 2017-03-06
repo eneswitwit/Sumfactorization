9650f190b8b9d602b15486433dbb657bee402256
marcelduerr <marcel.duerr@yahoo.de>
2017-03-06 14:40:40 +0100

    integrate will precompute all necessary values instead of computing them when needed.

--- Enes/Sumfactorization/Sumfactorization/include/integrate.h
+++ Enes/Sumfactorization/Sumfactorization/include/integrate.h
@@ -5,7 +5,7 @@ template <int order, typename y_type, template<int, typename> class Polynomial, 
 class Integrate {
 public:
 
-  constexpr int integrate_lagrange(std::array < std::array < y_type, order + 1 >, order + 1 > &y, const std::array < std::array < y_type, order + 1 >, order + 1 > &u) const {
+  constexpr int vmult_mass(std::array < std::array < y_type, order + 1 >, order + 1 > &y, const std::array < std::array < y_type, order + 1 >, order + 1 > &u) const {
     Quadrature<order, y_type> quad = {1.};
     quad.compute_quadrature_points(order + 1, 1, 1);
     quad.compute_quadrature_weights(quad.knots, 0, 0);
@@ -14,41 +14,122 @@ public:
     std::array < y_type, order + 1 > weights = quad.weights;
 
     Polynomial <order, y_type> lagr;
+
+    // evaluate all basis functions in 1D at all quadrature points.
+    // row index: index of basis function
+    // column index: index of quadrature point
+
+    std::array < std::array < y_type, order + 1 >, order + 1 > eval_poly;
+
+    for (unsigned int i=0;i<order+1;i++) {
+            for (unsigned int j=0;j<order+1;j++) {
+                    eval_poly[i][j]=lagr.eval_lagrange(i, quad.knots[j], quad.knots);
+                }
+        }
+
     int counter = 0;
     for (int k = 0; k <= order; k++)
-    {
-      for (int l = 0; l <= order; l++)
-      {
-        y_type val_qx = 0;
-        for (int qx = 0; qx <= order; qx++)
         {
-          y_type val_i = 0;
-          for (int i = 0; i <= order; i++)
-          {
-            y_type val_qy = 0;
-            for (int qy = 0; qy <= order; qy++)
-            {
-              y_type val_j = 0;
-              for (int j = 0; j <= order; j++)
-              {
-                val_j += lagr.eval_lagr(j, quad.knots[qy], quad.knots) * u[i][j];
-              }
-              val_qy += val_j * quad.weights[qy] * lagr.eval_lagr(l, quad.knots[qy], quad.knots);
-              counter++;
-              counter++;
-            }
-            val_i += val_qy * lagr.eval_lagr(i, quad.knots[qx], quad.knots);
-            counter++;
-          }
-          val_qx += val_i * quad.weights[qx] * lagr.eval_lagr(k, quad.knots[qx], quad.knots);
-          counter++;
-          counter++;
+            for (int l = 0; l <= order; l++)
+                {
+                    y_type val_qx = 0;
+                    for (int qx = 0; qx <= order; qx++)
+                        {
+                            y_type val_i = 0;
+                            for (int i = 0; i <= order; i++)
+                                {
+                                    y_type val_qy = 0;
+                                    for (int qy = 0; qy <= order; qy++)
+                                        {
+                                            y_type val_j = 0;
+                                            for (int j = 0; j <= order; j++)
+                                                {
+                                                    val_j += eval_poly[j][qy] * u[i][j];
+                                                }
+                                            val_qy += val_j * quad.weights[qy] * eval_poly[l][qy];
+                                            counter++;
+                                            counter++;
+                                        }
+                                    val_i += val_qy * eval_poly[i][qx];
+                                    counter++;
+                                }
+                            val_qx += val_i * quad.weights[qx] * evalpoly[k][qx];
+                            counter++;
+                            counter++;
+                        }
+                    y[k][l] = val_qx;
+                }
         }
-        y[k][l] = val_qx;
-      }
-    }
     return counter;
-  };
+  }
+
+  constexpr int vmult_laplacian(std::array < std::array < y_type, order + 1 >, order + 1 > &y, const std::array < std::array < y_type, order + 1 >, order + 1 > &u) const {
+      Quadrature<order, y_type> quad = {1.};
+      quad.compute_quadrature_points(order + 1, 1, 1);
+      quad.compute_quadrature_weights(quad.knots, 0, 0);
+
+      std::array < y_type, order + 1 > knots = quad.knots;
+      std::array < y_type, order + 1 > weights = quad.weights;
+
+      Polynomial <order, y_type> lagr;
+
+      // evaluate all basis functions and their derivatives in 1D at all quadrature points.
+      // row index: index of basis function
+      // column index: index of quadrature point
+      // third index: stores values for 0, stores values of gradient for 1
+
+      // dimension will be a template parameter later on
+      unsigned int dim=2;
+
+      std::array < std::array < std::array < y_type, order + 1 >, order + 1 >, 2 > eval_poly;
+
+      for (unsigned int i=0;i<order+1;i++) {
+              for (unsigned int j=0;j<order+1;j++) {
+                      eval_poly[i][j][0]=lagr.eval_lagrange(i, quad.knots[j], quad.knots);
+                      eval_poly[i][j][1]=lagr.eval_gradient(i, quad.knots[j], quad.knots);
+                  }
+          }
+      y_type val_qx, val_qy, val_i, val_j;
+
+      int counter = 0;
+      for (int k = 0; k <= order; k++)
+          {
+              for (int l = 0; l <= order; l++)
+                  {
+                      y[k][l]=0;
+                      for (unsigned int d=0;d<dim;d++) {
+                              val_qx = 0;
+                              for (int qx = 0; qx <= order; qx++)
+                                  {
+                                      val_i = 0;
+                                      for (int i = 0; i <= order; i++)
+                                          {
+                                              val_qy = 0;
+                                              for (int qy = 0; qy <= order; qy++)
+                                                  {
+                                                      val_j = 0;
+                                                      for (int j = 0; j <= order; j++)
+                                                          {
+                                                              val_j += eval_poly[j][qy][dim-1-d] * u[i][j];
+                                                          }
+                                                      val_qy += val_j * quad.weights[qy] * eval_poly[l][qy][dim-1-d];
+                                                      counter++;
+                                                      counter++;
+                                                  }
+                                              val_i += val_qy * eval_poly[i][qx][d];
+                                              counter++;
+                                          }
+                                      val_qx += val_i * quad.weights[qx] * evalpoly[k][qx][d];
+                                      counter++;
+                                      counter++;
+                                  }
+                              y[k][l] += val_qx;
+                          }
+                  }
+          }
+      return counter;
+  }
+
 };
 
 #endif
--- Enes/Sumfactorization/Sumfactorization/include/polynomial.h
+++ Enes/Sumfactorization/Sumfactorization/include/polynomial.h
@@ -8,24 +8,39 @@ public:
     constexpr y_type eval_lagr(int i, y_type x, const std::array < y_type, order + 1 > & knots) const {
         y_type val = 1.;
         for (int j = 0; j <= order; j++) {
-            if (i != j) {
-                val *= (x - knots[j]) / (knots[i] - knots[j]);
+                if (i != j) {
+                        val *= (x - knots[j]) / (knots[i] - knots[j]);
+                    }
             }
-        }
         return val;
     }
+    constexpr y_type eval_gradient(int i, y_type x, std::array < y_type, order + 1 > & knots) const {
+        y_type sum=0;
+        if(std::find(knots.begin(), knots.end(), x) != knots.end()) {
+                // evaluate gradient on a knot
+                for (unsigned int m=0;m<order+1;m++) {
+                        if (m!=i) {
+                                y_type product=1;
+                                for (unsigned int j=0;j<order+1;j++) {
+                                        if ((j!=i) && (j!=m)) {
+                                                product*=(x-knots[j])/(knots[i]-knots[j]);
+                                            }
+                                    }
+                                sum+=(knots[i]-knots[m])*product;
+                            }
+                    }
+                return sum;
+            } else {
+                // evaluate gradient in between knots
+                for (unsigned int j=0;j<order+1;j++) {
+                        if (j!=i) {
+                                sum+=1/(x-knots[j]);
+                            }
+                    }
+                return sum*eval_lagr(i,x,& knots);
+            }
 
-    constexpr y_type approx_gradient(int i, y_type x, std::array < y_type, order + 1 > & knots) const {
-        y_type h = 0;
-        h = sqrt(std::nextafter(h, h));
-        if (x != 0) {
-            h *= x;
-        }
-        y_type val = (eval_lagr(i, x + h, & knots) - eval_lagr(i, x + h, & knots)) / (2 * h);
-
-        return val;
     }
-
 
 };
 
