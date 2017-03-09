7cb76cd95a8ed7d297fe12602b61973c029a245f
Enes Witwit <Witwit@dhcp651-047.laptop-wlc.uni-heidelberg.de>
2017-03-09 20:47:20 +0100

    Changed directory strucure and CMakeLists.txt

--- Enes/Sumfactorization/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/Witwit/Github/polynomialspace/Enes/Sumfactorization/Sumfactorization
-# Build directory: /Users/Witwit/Github/polynomialspace/Enes/Sumfactorization
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(SumfactorizationRuns "Sumfactorization")
---
+++ Enes/Sumfactorization/Sumfactorization/build/CMakeFiles/testvmult.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testvmult.dir/tests/integrate.cc.o"
+  "../bin/testvmult.pdb"
+  "../bin/testvmult"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testvmult.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
