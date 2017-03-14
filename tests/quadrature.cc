3d4a38f6912b38fbfe76f4a42e9e9f49d6e6409a
Enes Witwit <Witwit@dhcp565-035.laptop-wlc.uni-heidelberg.de>
2017-03-14 13:34:32 +0100

    Quadrature_constexpr does not work.

---
+++ tests/quadrature.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <limits>
+#include <cmath>
+#include <cassert>
+#include "../include/la_operations.h"
+#include "../include/quadrature_constexpr.h"
+#include "../include/polynomial.h"
+#include "../include/vmult.h"
+#include "../include/integrate.h"
+
+
+
+
+int main(){
+    Quadrature<3,long double> quad;
+
+    return 0;
+}
