4096fee0e0b709cc595dc97b4074c80527782f96
Enes Witwit <witwitenes@gmail.com>
2017-03-21 11:53:10 +0100

    Updated Polynomial to constexpr!

--- include/polynomial.h
+++ include/polynomial.h
@@ -14,72 +14,9 @@ public:
         return quad.compute_quadrature_points();
     }
 
-    /*void compute_interpolation_points()
-    {
-      const unsigned int m = order - 1;
-      const long double
-      long_double_eps = static_cast<y_type>(std::numeric_limits<long double>::epsilon()),
-      double_eps      = static_cast<y_type>(std::numeric_limits<double>::epsilon());
-      const y_type runtime_one = 1.0;
-      const y_type tolerance = (runtime_one + long_double_eps != runtime_one ? std::max (double_eps / 100, long_double_eps * 5) : double_eps * 5 );
 
-      for (unsigned int i = 0; i < m; ++i)
-      {
-        knots[i + 1] = - std::cos( (y_type) (2 * i + 1) / (2 * m) * PI );
-      }
-      y_type s, J_x, f, delta;
-
-      for (unsigned int k = 1; k < m; ++k)
-      {
-        y_type r = knots[k];
-        if (k > 1)
-          r = (r + knots[k - 1]) / 2;
-
-        do
-        {
-          s = 0.;
-          for (unsigned int i = 1; i < k; ++i)
-            s += 1. / (r - knots[i]);
-
-          J_x   =  0.5 * (alpha + beta + m + 1) * JacobiP(r, alpha + 1, beta + 1, m - 1);
-          f     = JacobiP(r, alpha, beta, m);
-          delta = f / (f * s - J_x);
-          r += delta;
-        }
-        while (std::fabs(delta) >= tolerance);
-
-        knots[k] = r;
-      }
-      knots[0] = -1.L;
-      knots[order] = +1.L;
-      for (unsigned int j = 0; j < order + 1; j++) {
-        knots[j] *= 0.5;
-        knots[j] += 0.5;
-      }
-    }
-
-    y_type JacobiP(const y_type x, const int alpha, const int beta, const unsigned int n)
-    {
-      std::vector<y_type> p(n + 1);
-      p[0] = 1.0L;
-      if (n == 0) return p[0];
-      p[1] = ((alpha + beta + 2) * x + (alpha - beta)) / 2;
-      if (n == 1) return p[1];
-
-      for (unsigned int i = 1; i <= (n - 1); ++i)
-      {
-        const int v  = 2 * i + alpha + beta;
-        const int a1 = 2 * (i + 1) * (i + alpha + beta + 1) * v;
-        const int a2 = (v + 1) * (alpha * alpha - beta * beta);
-        const int a3 = v * (v + 1) * (v + 2);
-        const int a4 = 2 * (i + alpha) * (i + beta) * (v + 2);
-
-        p[i + 1] = static_cast<y_type>( (a2 + a3 * x) * p[i] - a4 * p[i - 1]) / a1;
-      }
-      return p[n];
-    }*/
-
-    y_type eval_lagrange(int i, y_type x) const {
+    template <y_type * x, unsigned int i>
+    constexpr y_type eval_lagrange() const {
         y_type val = 1.;
         for (int j = 0; j <= order; j++) {
                 if (i != j) {
@@ -89,7 +26,8 @@ public:
         return val;
     }
 
-    y_type eval_1st_derivative(int i, y_type x) const {
+    template <y_type * x, unsigned int i>
+    constexpr y_type eval_1st_derivative() const {
         y_type sum=0;
         for (unsigned int m=0;m<order+1;m++) {
                 if (m!=i) {
@@ -105,7 +43,8 @@ public:
         return sum;
     }
 
-    y_type eval_2nd_derivative(int i, y_type x) const {
+    template <y_type * x, unsigned int i>
+    constexpr y_type eval_2nd_derivative() const {
         y_type sum=0;
         for (unsigned int k=0;k<order+1;k++) {
                 if (k!=i) {
