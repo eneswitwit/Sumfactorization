eda752d30b0403cf0fa34863cec301ec7518dceb
Enes Witwit <witwitenes@gmail.com>
2017-03-26 13:26:02 +0200

    Added Eval Lagrange Laplacian Tests.

--- tests/polynomialbasis/lagrange.cc
+++ tests/polynomialbasis/lagrange.cc
@@ -30,7 +30,6 @@ int main() {
 
 
     /** TEST EVAL LAGRANGE **/
-
     // If we evaluate at node points we expect 1
     for (unsigned int i = 0; i < order + 1; i++) {
         assert( abs(lagr.eval_lagrange(lagr.knots_[i], i)-1) <= eps);
@@ -67,7 +66,27 @@ int main() {
     assert( abs(lagr.eval_1st_derivative(lagr.knots_[2],3) - 1.61804) <= eps );
     assert( abs(lagr.eval_1st_derivative(lagr.knots_[3],3) - 6) <= eps );
 
-
+    /** TEST EVAL LAPLACIAN **/
+    // 1st Lagrange Basis
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[0],0) - 20) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[1],0) - 11.7082) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[2],0) + 1.70821) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[3],0) + 10) <= eps );
+    // 2nd Lagrange Basis
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[0],1) + 38.541) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[1],1) + 20.0001) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[2],1) - 9.99986) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[3],1) - 28.5408) <= eps );
+    // 3rd Lagrange Basis
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[0],2) - 28.541) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[1],2) - 10.0001) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[2],2) + 19.9999) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[3],2) + 38.5408) <= eps );
+    // 4rd Lagrange Basis
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[0],3) + 10) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[1],3) + 1.70821) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[2],3) - 11.7082) <= eps );
+    assert( abs(lagr.eval_2nd_derivative(lagr.knots_[3],3) - 20) <= eps );
 
 
     return 0;
